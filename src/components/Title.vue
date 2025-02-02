972140c0e4986a298e1024934771764493fcccfd
Tom Plant <tom@tplant.com.au>
2025-02-02 13:05:51 +0000

    feat: dark mode with VueUse, better font
    Signed-off-by: GitHub <noreply@github.com>

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,7 +1,7 @@
 <template>
     <el-row style="margin-top: 20px; margin-left: 20px">
         <el-text style="font-size: 20px; margin-left: 20px">
-            <el-icon><i-ep-office-building /></el-icon>
+            <el-icon><i-ep-document /></el-icon>
             ADMX Viewer
         </el-text>
     </el-row>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 import App from './App.vue'
 import router from './router'
 
