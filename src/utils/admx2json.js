a6bf383de23042be986e82423674a62e88914a9e
Tom Plant <tom@tplant.com.au>
2025-02-05 01:35:38 +1100

    feat: automatically add third-party ADMX files (#2)
    based on EvergreenAdmx and the existing import functions. 6MB JSON probably won't scale for frontend search, especially localised
    
    Signed-off-by: Tom Plant <tom@tplant.com.au>

---
+++ src/utils/admx2json.js
@@ -0,0 +1,43 @@
+import fs from 'fs'
+import path, { join } from 'path'
+import { convert } from './admx.js'
+
+// adapted from TemplateMgmt.vue
+const processFolder = async (cwd, files, res) => {
+    for (const file of files) {
+        console.log('Processing', file)
+        if (
+            file.toLowerCase().endsWith('.admx') ||
+            file.toLowerCase().endsWith('.adml')
+        ) {
+            const text = fs.readFileSync(join(cwd, file), 'utf16le').toString()
+            res.push({
+                name: file,
+                content: text
+            })
+        }
+    }
+}
+
+const folderPath = process.argv[2]
+
+if (!folderPath) {
+    console.error('Please provide a folder path')
+    process.exit(1)
+}
+
+try {
+    const results = []
+    const files = fs.readdirSync(folderPath, { recursive: true })
+    await processFolder(folderPath, files, results)
+
+    const converted = convert(results)
+    const outputPath = path.join(process.cwd(), '..', 'public', 'admx.json')
+    console.log(converted)
+
+    fs.writeFileSync(outputPath, JSON.stringify(converted))
+    console.log(`Conversion complete. Output written to ${outputPath}`)
+} catch (error) {
+    console.error('Error:', error.message)
+    process.exit(1)
+}
